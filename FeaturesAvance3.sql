6b43c77d1370f9d4f0b8c4ecde11882a59f71965
CAMILO-N <camolonunez@outlook.com>
2026-04-28 14:52:33 -0500

    Minor changes for Database and Modeler, ready for 3rd delivery

--- FeaturesAvance3.sql
+++ FeaturesAvance3.sql
@@ -1,8 +1,16 @@
-
-
-
 use SonoraInc
 go
 
+/* 
+   Este script complementa el script generado por Data Modeler.
+   Incluye ajustes, indices, seguridad, usuarios, procedimientos,
+   funciones, triggers, cursores y consultas de reportes.
+  */
 
-print 'HolaAlejo'
+
+
+
+
+
+
+
